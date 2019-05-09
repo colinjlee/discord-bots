c11e5f1195548bd353ee64d258a71691a03e8e7e
colinjlee <50276805+colinjlee@users.noreply.github.com>
2019-05-09 00:58:35 -0700

    Added more functions
    Added: philosopher book rolls, marvel machine rolls, philo/marvel rolls until a specific item is found, single flip that merged flip and flip guess, show stats of philo/marvel runs, save stats of lucky items from philo/marvel

--- gamble_bot/gamble_bot.py
+++ gamble_bot/gamble_bot.py
@@ -13,10 +13,44 @@ async def on_ready():
 #List of commands for the bot
 @bot.command()
 async def halp(ctx):
-    msg = """Arguments with \* are optional
-g!flip  [\*numFlips]: Flips a coin 'numFlips' amount of times
+    msg = """Arguments with \* are optional, they come with default values
+g!flip [\*numFlips]: Flips a coin 'numFlips' amount of times
 g!flip2 [guess]: Flip a coin with a guess of 'h' or 't'
-g!roll  [\*numRolls] [\*numSides]: Rolls a die with 'numSides' amount of sides 'numRolls' amount of times"""
+g!roll [\*numRolls] [\*numSides]: Rolls a die with 'numSides' amount of sides 'numRolls' amount of times
+g!philo [\*numRolls] [\*findItem]: Open 'numRolls' amount of philosopher books or open until 'findItem' is rolled
+g!marvel [\*numRolls] [\*findItem]: Roll the marvel machine 'numRolls' amount of times or roll until 'findItem' is rolled
+g!stats [\*statsType]: Get your stats for (1) philo and marvel or (2) blackjack
+g!reset [statsType]: Reset your stats for (1) philo (2) marvel (3) blackjack or (4) all"""
+    await ctx.send(msg)
+
+#Get user stats for marvel and philo or blackjack
+#(1) for marvel/philo and (2) for blackjack, with default value of 1
+@bot.command()
+async def stats(ctx, statsType:typing.Optional[int] = 1):
+    #Get user's name and unique ID
+    userName = ctx.author.name
+    userID = str(ctx.author.id)
+
+    if statsType == 1:
+        msg = gamble_bot_methods.pmStats(userName, userID)
+    elif statsType == 2:
+        msg = gamble_bot_methods.bjStats(userName, userID)
+    else:
+        msg = "Invalid type"
+    await ctx.send(msg)
+
+#Reset user stats for specified data
+#(1) philo (2) marvel (3) blackjack or (4) all
+@bot.command()
+async def reset(ctx, statsType:int = None):
+    #Get user's name and unique ID
+    userName = ctx.author.name
+    userID = str(ctx.author.id)
+
+    if statsType == 1 or statsType == 2 or statsType == 3 or statsType == 4:
+        msg = gamble_bot_methods.resetStats(userName, userID, statsType)
+    else:
+        msg = "Invalid type"
     await ctx.send(msg)
 
 #Check the bot ping
@@ -27,16 +61,21 @@ async def ping(ctx):
     await ctx.send("{}s".format(rounded))
 
 #Flip a fair coin up to 100,000 times at once with default flip amount of 1
+#Or flip a coin once with an initial guess
+#Note that giving both arguments calls flipGuess
 @bot.command()
-async def flip(ctx, numFlips:typing.Optional[int] = 1):
-    msg = gamble_bot_methods.flip(numFlips)
+async def flip(ctx, numFlips:typing.Optional[int] = 1, *, guess:str = None):
+    if guess == None:
+        msg = gamble_bot_methods.flip(numFlips)
+    else:
+        msg = gamble_bot_methods.flipGuess(guess)
     await ctx.send(msg)
 
 #Flip a fair coin once with an initial guess
-@bot.command()
-async def flip2(ctx, guess:str = ""):
-    msg = gamble_bot_methods.flipGuess(guess)
-    await ctx.send(msg)
+# @bot.command()
+# async def flip2(ctx, guess:str = ""):
+#     msg = gamble_bot_methods.flipGuess(guess)
+#     await ctx.send(msg)
 
 #Roll a die with default number of sides of 6 and roll amount of 1
 #Maximum number of sides of 100 and roll amount of 100,000
@@ -45,6 +84,42 @@ async def roll(ctx, numRolls:typing.Optional[int] = 1, numSides:typing.Optional[
     #Note bots have max length of 2,000 chars per msg
     results = gamble_bot_methods.rollDie(numRolls, numSides)
     for msg in results:
+        await ctx.send(msg)
+
+#Roll the specified amount of philosopher books, up to 165
+#Or roll for a specific item
+#Note that giving both arguments calls philoFind
+@bot.command()
+async def philo(ctx, numRolls:typing.Optional[int] = 11, *, find_item:str = None):
+    #Get user's name and unique ID
+    userName = ctx.author.name
+    userID = str(ctx.author.id)
+
+    if find_item == None:
+        results = gamble_bot_methods.philoRoll(userName, userID, numRolls)
+        for msg in results:
+            await ctx.send(msg)
+    else:
+        #These rolls are not recorded in stats
+        msg = gamble_bot_methods.philoFind(find_item)
+        await ctx.send(msg)
+
+#Roll marvel marchine the specified amount of times, up to 110
+#Or roll for a specific item
+#Note that giving both arguments calls marvelFind
+@bot.command()
+async def marvel(ctx, numRolls:typing.Optional[int] = 11, *, find_item:str = None):
+    #Get user's name and unique ID
+    userName = ctx.author.name
+    userID = str(ctx.author.id)
+
+    if find_item == None:
+        results = gamble_bot_methods.marvelRoll(userName, userID, numRolls)
+        for msg in results:
+            await ctx.send(msg)
+    else:
+        #These rolls are not recorded in stats
+        msg = gamble_bot_methods.marvelFind(find_item)
         await ctx.send(msg)
 
 #Read in the unique bot ID and run bot
--- gamble_bot/gamble_bot_methods.py
+++ gamble_bot/gamble_bot_methods.py
@@ -1,4 +1,430 @@
 import random
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+#Google sheets things
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+
+#Google sheets containing user marvel/philo stats
+philoStatSheet = client.open("Gamble Bot User Stats").get_worksheet(0)
+philoItemRow = philoStatSheet.row_values(1)
+marvelStatSheet = client.open("Gamble Bot User Stats").get_worksheet(1)
+marvelItemRow = marvelStatSheet.row_values(1)
+
+#Make a mapping for marvel and philo items to column number
+marvelItemMapping = {}
+for i in range(2, len(marvelItemRow)):
+    marvelItemMapping[marvelItemRow[i]] = i+1
+
+philoItemMapping = {}
+for i in range(2, len(philoItemRow)):
+    philoItemMapping[philoItemRow[i]] = i+1
+
+#Google sheets containing user blackjack stats
+#statSheet2 = client.open("Gamble Bot User Stats").get_worksheet(3)
+
+#Google sheets containing public rates of philosopher books and marvel machine
+#Philo rates: http://maplestory.nexon.net/micro-site/42030
+#Marvel rates: http://maplestory.nexon.net/micro-site/39184
+philoSheet = client.open("Philosopher Book Rates").worksheets()
+philoSheet = philoSheet[-1]
+marvelSheet = client.open("Marvel Machine Rates").worksheets()
+marvelSheet = marvelSheet[-1]
+
+#Get the 2 lists to roll from for philo book items
+philoList1 = []
+philoList2 = []
+
+philoItemList1 = philoSheet.col_values(3)
+philoItemList2 = philoSheet.col_values(6)
+philoRateList1 = philoSheet.col_values(4)
+philoRateList2 = philoSheet.col_values(7)
+
+#Make lists containing the items proportionate to their rate
+for i in range(1, len(philoItemList1)):
+    #Make the items lowercase for easier matching
+    philoItemList1[i] = philoItemList1[i].lower()
+    item = philoItemList1[i]
+    #Make the %rate an integer with no decimal values
+    rate = float(philoRateList1[i][:-1])*100
+    #Add the item multiple times proportionate to their rate
+    philoList1 += [item]*int(rate)
+
+#Same as above for the 2nd list of items
+for i in range(1, len(philoItemList2)):
+    philoItemList2[i] = philoItemList2[i].lower()
+    item = philoItemList2[i]
+    rate = float(philoRateList2[i][:-1])*100
+    philoList2 += [item]*int(rate)
+
+#Similar procedure as above but for marvel items
+marvelList1 = []
+marvelList2 = []
+marvelList3 = []
+
+marvelItemList1 = marvelSheet.col_values(3)
+marvelItemList2 = marvelSheet.col_values(6)
+marvelItemList3 = marvelSheet.col_values(9)
+marvelRateList1 = marvelSheet.col_values(4)
+marvelRateList2 = marvelSheet.col_values(7)
+marvelRateList3 = marvelSheet.col_values(10)
+
+#Note that some item rates from list 1 and 2 of marvel go up to 3 decimal places
+for i in range(1, len(marvelItemList1)):
+    marvelItemList1[i] = marvelItemList1[i].lower()
+    item = marvelItemList1[i]
+    rate = float(marvelRateList1[i][:-1])*1000
+    marvelList1 += [item]*int(rate)
+
+for i in range(1, len(marvelItemList2)):
+    marvelItemList2[i] = marvelItemList2[i].lower()
+    item = marvelItemList2[i]
+    rate = float(marvelRateList2[i][:-1])*1000
+    marvelList2 += [item]*int(rate)
+
+for i in range(1, len(marvelItemList3)):
+    marvelItemList3[i] = marvelItemList3[i].lower()
+    item = marvelItemList3[i]
+    rate = float(marvelRateList3[i][:-1])*1000
+    marvelList3 += [item]*int(rate)
+
+#Calculate philo or marvel spendings
+#Return result through pair
+def pmSpendings(numRolls, numIndicator):
+    price = -1
+    #0 for philo, 1 for marvel
+    if numIndicator == 0:
+        price = 2.6
+    elif numIndicator == 1:
+        price = 4.9
+
+    #Calculate and return costs
+    low_money = int(numRolls/11)*int(price*10) + (numRolls%11)*price
+    high_money = numRolls*price
+    return (low_money, high_money)
+
+#Get user's row that has their stats, or initialize them if user doesn't exist in data
+#Return the row numbers as a pair, first being philo stats then marvel
+def getUserRow(userName, userID):
+    global marvelStatSheet, philoStatSheet, philoItemRow, marvelItemRow
+    #Get list of user IDs and search for userID
+    userIDList1 = philoStatSheet.col_values(2)
+    userIDList2 = marvelStatSheet.col_values(2)
+    userRow1 = -1
+    userRow2 = -1
+
+    #Data is currently not sorted in any way so just linear search
+    for i in range(0, len(userIDList1)):
+        if userID == userIDList1[i]:
+            userRow1 = i+1
+
+    for i in range(0, len(userIDList2)):
+        if userID == userIDList2[i]:
+            userRow2 = i+1
+
+    #Update name if different from last time
+    if userRow1 != -1:
+        userStats = philoStatSheet.row_values(userRow1)
+        if userName != userStats[0]:
+            philoStatSheet.update_cell(1,userRow1, userName)
+    #Add a new row for the new user in philo stats
+    else:
+        newUser = [userName, userID]
+        newUser += ["0"]*(len(philoItemRow)-2)
+        philoStatSheet.append_row(newUser)
+        userRow1 = len(userIDList1)+1
+    #Update name if different from last time
+    if userRow2 != -1:
+        userStats = marvelStatSheet.row_values(userRow2)
+        if userName != userStats[0]:
+            marvelStatSheet.update_cell(1,userRow2, userName)
+    #Add a new row for the new user in marvel stats
+    else:
+        newUser = [userName, userID]
+        newUser += ["0"]*(len(marvelItemRow)-2)
+        marvelStatSheet.append_row(newUser)
+        userRow2 = len(userIDList2)+1
+
+    return (userRow1, userRow2)
+
+#Update user's item stats for philo/marvel
+def updateUserItemStat(userRow, item, incAmount, numIndicator):
+    global philoStatSheet, marvelStatSheet, philoItemMapping, marvelItemMapping
+
+    #Update philo item
+    if numIndicator == 0:
+        itemLoc = philoItemMapping[item]
+        oldVal = int(philoStatSheet.cell(userRow, itemLoc).value)
+        philoStatSheet.update_cell(userRow, itemLoc, str(oldVal+incAmount))
+    #Update marvel item
+    else:
+        itemLoc = marvelItemMapping[item]
+        oldVal = int(marvelStatSheet.cell(userRow, itemLoc).value)
+        marvelStatSheet.update_cell(userRow, itemLoc, str(oldVal+incAmount))
+
+#Philo and marvel stats
+#Return result through string
+def pmStats(userName, userID):
+    global philoStatSheet, marvelStatSheet, philoItemRow, marvelItemRow
+
+    #Retrieve user stats and update userName if necessary
+    userRows = getUserRow(userName, userID)
+    userRowPhilo = userRows[0]
+    userRowMarvel = userRows[1]
+    userStatsPhilo = philoStatSheet.row_values(userRowPhilo)
+    userStatsMarvel = marvelStatSheet.row_values(userRowMarvel)
+    msg = "**__" + userName + "\'s marvel and philo stats__**\n\n"
+
+    #Currently hard coded values of cell locations for marvel/philo
+    #Philo stats
+    philoRolls = int(userStatsPhilo[2])
+    costs = pmSpendings(philoRolls, 0)
+    low_money = costs[0]
+    high_money = costs[1]
+    msg += "**__Philosopher Book Statistics:__**\nTotal amount of books: {} ".format(philoRolls)
+    msg += "(${:,.2f} ~ ${:,.2f})\n".format(low_money, high_money)
+
+    for i in range(3, len(philoItemRow)):
+        item = philoItemRow[i].title()
+        userAmount = int(userStatsPhilo[i])
+        if userAmount > 0:
+            msg += "{}: {:,d}\n".format(item, userAmount)
+
+    #Marvel stats
+    marvelRolls = int(userStatsMarvel[2])
+    costs = pmSpendings(marvelRolls, 1)
+    low_money = costs[0]
+    high_money = costs[1]
+    msg += "\n**__Marvel Machine Statistics:__**\nTotal amount of spins: {} ".format(marvelRolls)
+    msg += "(${:,.2f} ~ ${:,.2f})\n".format(low_money, high_money)
+
+    for i in range(3, len(marvelItemRow)):
+        item = marvelItemRow[i].title()
+        userAmount = int(userStatsMarvel[i])
+        if userAmount > 0:
+            msg += "{}: {:,d}\n".format(item, userAmount)
+
+    return msg
+
+#TODO: Blackjack stats
+#Return result through string
+def bjStats(userName, userID):
+    return("TODO")
+
+#TODO: reset stats
+#Return a string indicating success
+def resetStats(userName, userID, statsType):
+    return("TODO")
+
+#Returns a string of the notable items won from philo or marvel
+def notableItems(luckyList, gotLucky):
+    if gotLucky == True:
+        msg = "\n**__Notable items:__**\n"
+        for item in luckyList:
+            if luckyList[item] > 0:
+                msg += item.title() + ": " + str(luckyList[item]) + "\n"
+    else:
+        msg = "\n**__No notable items__**"
+    return msg
+
+#Roll a specified number of philosopher books, up to 165
+#Return the results through an array
+def philoRoll(userName, userID, numRolls):
+    global philoList1, philoList2
+
+    #Lucky items in philo
+    lucky = {"battle-roid (f) coupon":0, "battle-roid (m) coupon":0, "outlaw heart":0, "frenzy totem":0, "firestarter ring coupon":0, "wolf underling familiar":0}
+    gotLucky = False
+    messages = []
+    msg = ""
+
+    #Get user info to record stats
+    userRow = getUserRow(userName, userID)[0]
+
+    #Min roll of 1, max of 165 rolls
+    if numRolls < 1:
+        messages.append("Amount of rolls must be positive")
+        return messages
+    elif numRolls > 165:
+        msg += "**Max of 165 philo books (15 packs)**\n"
+        numRolls = 165
+
+    #Roll the specified amount of books
+    for i in range(0, numRolls):
+        temp = "**[" + str(i+1) + "]** "
+        item1 = random.choice(philoList1)
+        item2 = random.choice(philoList2)
+        #Discord syntax to bold and underline lucky items
+        if item1 in lucky:
+            temp += "**__" + item1.title() + "__**, "
+            lucky[item1] += 1
+            updateUserItemStat(userRow, item1, 1, 0)
+            gotLucky = True
+        else:
+            temp += item1.title() + ", "
+        if item2 in lucky:
+            temp += "**__" + item2.title() + "__** "
+            lucky[item2] += 1
+            updateUserItemStat(userRow, item2, 1, 0)
+            gotLucky = True
+        else:
+            temp += item2.title() + " "
+        #Check for bot message length limit of 2,000 chars
+        if len(msg) + len(temp) > 2000:
+            messages.append(msg)
+            msg = temp
+        else:
+            msg += temp
+
+    #Highlight lucky items and update user stat
+    temp = notableItems(lucky, gotLucky)
+    updateUserItemStat(userRow, "rolls", numRolls, 0)
+
+    #Another final check for message length limit
+    if len(msg) + len(temp) > 2000:
+        messages.append(msg)
+        msg = temp
+    else:
+        msg += temp
+    messages.append(msg)
+    return messages
+
+#Roll philo books until the specified item is found
+#These rolls are not recorded in stats
+#Return the results through a string
+def philoFind(item):
+    global philoList1, philoList2
+    counter = 0
+    found = False
+    item = item.strip().lower()
+
+    #Roll until item is found
+    if item in philoList1 or item in philoList2:
+        while found == False:
+            counter += 1
+            if item in philoList1:
+                result = random.choice(philoList1)
+            else:
+                result = random.choice(philoList2)
+            if item == result:
+                found = True
+    else:
+        return "Invalid item"
+
+    #Calculate spending costs and return results
+    costs = pmSpendings(counter, 0)
+    low_money = costs[0]
+    high_money = costs[1]
+    msg = "It took {:,d} book(s) to get the item: ".format(counter) + item.title() + "\n"
+    msg += "{:,d} book(s) = ${:,.2f} ~ ${:,.2f}".format(counter, low_money, high_money)
+    return msg
+
+#Roll marvel a specified number of times, up to 110
+#Return the results through an array
+def marvelRoll(userName, userID, numRolls):
+    global marvelList1, marvelList2, marvelList3
+
+    #Lucky items in marvel
+    lucky = {"1 mil maple points":0, "frenzy totem":0, "dark avenger totem":0, "dark doom totem":0, "dark grin totem":0, "dark hellia totem":0,
+    "lucid's earrings coupon":0, "firestarter ring":0, "the ring of torment coupon":0, "permanent pendant slot":0, "permanent hyper teleport rock coupon":0,
+    "battleroid (male)":0, "battleroid (female)":0, "outlaw heart":0, "100,000 maple points chip":0, "wolf underling familiar":0}
+    gotLucky = False
+    messages = []
+    msg = ""
+
+    #Get user info to record stats
+    userRow = getUserRow(userName, userID)[1]
+
+    #Min roll of 1, max roll of 110
+    if numRolls < 1:
+        messages.append("Amount of rolls must be positive")
+        return messages
+    elif numRolls > 110:
+        msg += "**Max of 110 marvel spins (10 packs)**\n"
+        numRolls = 110
+
+    #Roll the specified amount
+    for i in range(0, numRolls):
+        temp = "**[" + str(i+1) + "]** "
+        itemA = random.choice(marvelList1)
+        itemB = random.choice(marvelList2)
+        itemC = random.choice(marvelList3)
+        #Discord syntax to bold and underline lucky items
+        if itemA in lucky:
+            temp += "**__" + itemA.title() + "__**, "
+            lucky[itemA] += 1
+            updateUserItemStat(userRow, itemA, 1, 1)
+            gotLucky = True
+        else:
+            temp += itemA.title() + ", "
+        if itemB in lucky:
+            temp += "**__" + itemB.title() + "__**, "
+            lucky[itemB] += 1
+            updateUserItemStat(userRow, itemB, 1, 1)
+            gotLucky = True
+        else:
+            temp += itemB.title() + ", "
+        if itemC in lucky:
+            temp += "**__" + itemC.title() + "__** "
+            lucky[itemC] += 1
+            updateUserItemStat(userRow, itemC, 1, 1)
+            gotLucky = True
+        else:
+            temp += itemC.title() + " "
+        #Check for bot message length limit of 2,000 chars
+        if len(msg) + len(temp) > 2000:
+            messages.append(msg)
+            msg = temp
+        else:
+            msg += temp
+
+    #Highlight lucky items and update user stats
+    temp = notableItems(lucky, gotLucky)
+    updateUserItemStat(userRow, "rolls", numRolls, 1)
+
+    #Another final check for message length limit
+    if len(msg) + len(temp) > 2000:
+        messages.append(msg)
+        msg = temp
+    else:
+        msg += temp
+    messages.append(msg)
+    return messages
+
+#Roll marvel until the specified item is found
+#These rolls are not recorded in stats
+#Return results through a string
+def marvelFind(item):
+    global marvelList1, marvelList2, marvelList3
+    counter = 0
+    found = False
+    item = item.strip().lower()
+
+    #Roll until item is found
+    if item in marvelList1 or item in marvelList2 or item in marvelList3:
+        while found == False:
+            counter += 1
+            if item in marvelList1:
+                result = random.choice(marvelList1)
+            elif item in marvelList2:
+                result = random.choice(marvelList2)
+            else:
+                result = random.choice(marvelList3)
+            if item == result:
+                found = True
+    else:
+        return "Invalid item"
+
+    #Calculate spending costs and return results
+    costs = pmSpendings(counter, 1)
+    low_money = costs[0]
+    high_money = costs[1]
+    msg = "It took {:,d} spin(s) to get the item: ".format(counter) + item.title() + "\n"
+    msg += "{:,d} spin(s) = ${:,.2f} ~ ${:,.2f}".format(counter, low_money, high_money)
+    return msg
 
 #Roll a die with specified number of sides, a specified number of times
 #Return the results through an array
